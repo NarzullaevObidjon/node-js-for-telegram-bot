ec6ac9f4bf11251d61e0d6c22e1d2c1b7305038e
NarzullaevObidjon <obidsattarovich3600@gmail.com>
2023-04-09 22:45:17 +0500

    b

--- routes/vacancies.js
+++ routes/vacancies.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const axios = require('axios');
 
 router.get("/vacancies",(req,res)=>{
-    res.send("Here is vacancies")
+    const vacancies = {};
+    axios.get('https://ishapi.mehnat.uz/api/v1/vacancies?page=1&per_page=1').then((data)=>{
+        
+    })
 })
 
 module.exports = router

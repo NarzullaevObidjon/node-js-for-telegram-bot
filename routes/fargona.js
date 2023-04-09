9c45c4c8f589f0f9d6231de3db03e67efb067a4d
NarzullaevObidjon <obidsattarovich3600@gmail.com>
2023-04-10 00:11:38 +0500

    finish

---
+++ routes/fargona.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const axios = require('axios');
+const path = require('path');
+const https = require('https');
+const rootCas = require('ssl-root-cas').create();
+
+rootCas.addFile(path.resolve('./cer','intermediate.pem'));
+const httpsAgent = new https.Agent({ca: rootCas});
+
+router.get("/fargona",(req,res)=>{
+    axios.get('https://ishapi.mehnat.uz/api/v1/vacancies?page=1&per_page=20&sort_key=created_at&company_soato_code=1730',{ httpsAgent })
+    .then(function (response) {
+        res.send(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+})
+module.exports = router

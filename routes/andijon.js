06ccac1b47533c391be4092f6d68e499ed120ea1
NarzullaevObidjon <obidsattarovich3600@gmail.com>
2023-04-10 10:21:44 +0500

    sfgfege

--- routes/andijon.js
+++ routes/andijon.js
@@ -8,8 +8,8 @@ const rootCas = require('ssl-root-cas').create();
 rootCas.addFile(path.resolve('cer','intermediate.pem'));
 const httpsAgent = new https.Agent({ca: rootCas});
 
-router.get("/",(req,res)=>{
-    axios.get('https://ishapi.mehnat.uz/api/v1/vacancies?page=1&per_page=1&sort_key=created_at&company_soato_code=1703',{ httpsAgent })
+router.get("/andijon",(req,res)=>{
+    axios.get('https://ishapi.mehnat.uz/api/v1/vacancies?page=1&per_page=&20sort_key=created_at&company_soato_code=1703',{ httpsAgent })
     .then(function (response) {
         res.send(response.data);
       });
